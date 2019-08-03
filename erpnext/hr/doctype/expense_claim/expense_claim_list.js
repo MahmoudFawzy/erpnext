dc986100e8e4fa6464ea1fe90c184e6ee66b0e1d
Klenka <dev@itworkco.com>
2019-08-03 19:53:05 +0200

    1

--- erpnext/hr/doctype/expense_claim/expense_claim_list.js
+++ erpnext/hr/doctype/expense_claim/expense_claim_list.js
@@ -1,12 +1,16 @@
 frappe.listview_settings['Expense Claim'] = {
 	add_fields: ["total_claimed_amount", "docstatus"],
-	get_indicator: function(doc) {
-		if(doc.status == "Paid") {
+	get_indicator: function (doc) {
+		if (doc.status == "Paid") {
 			return [__("Paid"), "green", "status,=,'Paid'"];
-		}else if(doc.status == "Unpaid") {
+		} else if (doc.status == "Unpaid") {
 			return [__("Unpaid"), "orange"];
-		} else if(doc.status == "Rejected") {
+		} else if (doc.status == "Rejected") {
 			return [__("Rejected"), "grey"];
 		}
 	}
 };
+
+
+
+// /1111111111111
